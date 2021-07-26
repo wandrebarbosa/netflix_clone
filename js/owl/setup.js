792fa5ac359542870abe4488d28eff8931719820
wandrebarbosa <wandresonbarbosa@gmail.com>
2021-07-26 19:37:05 -0300

    up interface netflix

---
+++ js/owl/setup.js
@@ -0,0 +1,17 @@
+//configuração de carrousel com biblioteca jquery
+$('.owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
